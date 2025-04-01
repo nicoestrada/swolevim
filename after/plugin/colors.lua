00b9cad8142c9e7c54a6a72e237d576e0489e95f
Nico <estrada@hey.com>
2025-04-01 14:16:57 -0500

    colors

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -3,7 +3,7 @@ function ColorPump(color)
 	vim.cmd.colorscheme(color)
 
 --	vim.api.nvim_set_hl(0, "Normal", { bg = "none", ctermbg = "NONE" })
---	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none", ctermbg = "NONE" })
+--	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none", ctermbg = "NONE" }
 --	vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "none", ctermbg = "NONE" })
 --	vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "none", ctermbg = "NONE" })
 end
