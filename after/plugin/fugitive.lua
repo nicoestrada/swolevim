5b87c617d7ddd995eb11b70752b98a1c12308d42
Nico <estrada@hey.com>
2025-04-02 14:39:08 -0500

    git keymaps

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -8,3 +8,19 @@ end,
 });
 
 
+vim.keymap.set("n", "<leader>gc", function()
+    local message = vim.fn.input("Commit message: ")
+    if message ~= "" then
+        vim.cmd('Git commit -m "' .. message .. '"')
+    end
+end,
+{
+    noremap = true,
+});
+
+vim.keymap.set("n", "<leader>gp", function()
+    vim.cmd("Git push origin master")
+end,
+{
+    noremap = true,
+});
